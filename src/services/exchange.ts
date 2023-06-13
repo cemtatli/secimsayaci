758d422d7d842da723487ce3cc8cb2666b58e9af
cemtatli <cemtatli3535@gmail.com>
2023-06-13 18:11:33 +0300

    fix ?

--- src/services/exchange.ts
+++ src/services/exchange.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getExchangeRates = async (): Promise<{ [key: string]: any }> => {
+export const getExchangeRates = async (): Promise<{ [key: string]: unknown }> => {
   try {
-    const response = await axios.get("http://hasanadiguzel.com.tr/api/kurgetir");
+    const response = await axios.get("https://hasanadiguzel.com.tr/api/kurgetir");
     return response.data.TCMB_AnlikKurBilgileri;
   } catch (error) {
     throw new Error("Döviz kurları alınırken bir hata oluştu.");
