59f8c989a1ae52879cf95cb4af81377e191852c1
Cem <65294379+cemtatli@users.noreply.github.com>
2023-07-05 15:43:51 +0300

    Update header deleted sticky

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import ThemeChanger from "./theme-changer";
 
 const Header = () => {
   return (
-    <header className="sticky top-2 flex h-15 items-center justify-center rounded-full border z-30 bg-white dark:bg-zinc-900 border-zinc-200 backdrop-blur-sm dark:border-opacity-10">
+    <header className="flex h-15 items-center justify-center rounded-full border z-30 bg-white dark:bg-zinc-900 border-zinc-200 backdrop-blur-sm dark:border-opacity-10">
       <nav className="flex h-full w-full items-center justify-between px-2">
         <h3 className="select-none px-2 text-xl font-semibold dark:text-white text-slate-950">
           Seçim Sayacı
