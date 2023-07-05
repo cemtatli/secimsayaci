1141705efab0bbca9cf1f62480c60e93c1221244
Cem <65294379+cemtatli@users.noreply.github.com>
2023-07-05 15:47:43 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import ThemeChanger from "./theme-changer";
 
 const Header = () => {
   return (
-    <header className="flex h-15 items-center justify-center border bg-white dark:bg-zinc-900 border-zinc-200 backdrop-blur-sm dark:border-opacity-10">
+    <header className="flex h-15 items-center justify-center bg-white dark:bg-zinc-900 backdrop-blur-sm">
       <nav className="flex h-full w-full items-center justify-between px-2">
         <h3 className="select-none px-2 text-xl font-semibold dark:text-white text-slate-950">
           Seçim Sayacı
